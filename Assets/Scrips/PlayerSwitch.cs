6d8fb51633061beba9abe27f027f37b2b9816be7
nabetse1320 <petermix.were@gmail.com>
2023-07-29 17:11:11 -0500

    animator
    desabilitar animator para pausar personaje

--- Assets/Scrips/PlayerSwitch.cs
+++ Assets/Scrips/PlayerSwitch.cs
@@ -9,6 +9,20 @@ public class PlayerSwitch : MonoBehaviour
     public Transform Player1;
     public Transform Player2;
     public CinemachineVirtualCamera virtualCamera;
+
+    public GameObject p1;
+    public GameObject p2;
+    public Material skin;
+
+
+    private void Start()
+    {
+        Material materialP2 = p2.GetComponent<Renderer>().material;
+        Color colorP2 = materialP2.color;
+        colorP2.a = 0.5f;
+        materialP2.color = colorP2;
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))
@@ -37,24 +51,46 @@ public class PlayerSwitch : MonoBehaviour
         if (player1Active)
         {
             player1.isActive = false;
-            player1.animator.enabled = false; // Desactivar el Animator o detener animaciones
+            player1.animator.enabled = false;
             player2.isActive = true;
-            player2.animator.enabled = true; // Activar el Animator
+            player2.animator.enabled = true;
             player1.enabled = false;
             player2.enabled = true;
             player1Active = false;
             virtualCamera.Follow = Player2; // Cambiar el objetivo de la cámara al personaje 2
+
+
+            Material materialP1 = p1.GetComponent<Renderer>().material;
+            Color colorP1 = materialP1.color;
+            colorP1.a = 0.5f;
+            materialP1.color = colorP1;
+
+            Material materialP2 = p2.GetComponent<Renderer>().material;
+            Color colorP2 = materialP2.color;
+            colorP2.a = 1.0f;
+            materialP2.color = colorP2;
+
         }
         else
         {
             player1.isActive = true;
             player1.animator.enabled = true; // Activar el Animator
             player2.isActive = false;
-            player2.animator.enabled = false; // Desactivar el Animator o detener animaciones
+            player2.animator.enabled = false;
             player1.enabled = true;
             player2.enabled = false;
             player1Active = true;
             virtualCamera.Follow = Player1; // Cambiar el objetivo de la cámara al personaje 1
+
+            Material materialP2 = p2.GetComponent<Renderer>().material;
+            Color colorP2 = materialP2.color;
+            colorP2.a = 0.5f;
+            materialP2.color = colorP2;
+
+            Material materialP1 = p1.GetComponent<Renderer>().material;
+            Color colorP1 = materialP1.color;
+            colorP1.a = 1.0f;
+            materialP1.color = colorP1;
         }
 
     }

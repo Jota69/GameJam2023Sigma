558aa9b28d4cdbf34dd745f5b4d597ad52d6caf5
nabetse1320 <petermix.were@gmail.com>
2023-07-30 05:46:19 -0500

    f
    df

--- Assets/Scrips/Enemigo1.cs
+++ Assets/Scrips/Enemigo1.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using static UnityEngine.InputSystem.Controls.AxisControl;
 
 public class Enemigo : MonoBehaviour
 {
@@ -12,6 +14,7 @@ public class Enemigo : MonoBehaviour
     private bool muerto;
     private bool isGrounded;
     private bool atacando;
+    private bool golpeEjecutado = false;
 
     private Vector3 vectorPosicionRaycast;
     [SerializeField] private int vida;
@@ -70,18 +73,32 @@ public class Enemigo : MonoBehaviour
         return (hit.collider != null);
     }
 
-    private void atacar() 
+    private void atacar()
     {
+        
         atacando = true;
+
         StartCoroutine(ataque());
         
     }
+
     private IEnumerator ataque()
     {
-        animator.SetBool("ataque",true);
-        yield return new WaitForSeconds(AnimacionAtaque.length);
+        //animator.SetBool("ataque",true);
+        yield return new WaitForSeconds(3f /*AnimacionAtaque.length*/);
+
+        if (!golpeEjecutado) // Verificar nuevamente antes de ejecutar el golpe
+        {
+            golpeEjecutado = true;
+            Golpe();
+            ; // Marcar el golpe como ejecutado
+        }
+       
         atacando = false;
+        yield return new WaitForSeconds(1f);
+        golpeEjecutado = false;
     }
+
     private void Golpe()
     {
         Collider2D[] objetos = Physics2D.OverlapCircleAll(controladorGolpe.position, radioGolpe);
@@ -93,6 +110,13 @@ public class Enemigo : MonoBehaviour
             }
         }
     }
+    
+
+
+
+
+
+
 
 
 
@@ -128,6 +152,11 @@ public class Enemigo : MonoBehaviour
     {
         Vector2 raycastOrigin = inicioRaycastGround.transform.position;
         Debug.DrawRay(raycastOrigin, Vector2.down * raycastDistancia2, Color.red);
+    }
+    private void OnDrawGizmos()
+    {
+       Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(controladorGolpe.position, radioGolpe);
     }
 
     //private void OnDrawGizmos()

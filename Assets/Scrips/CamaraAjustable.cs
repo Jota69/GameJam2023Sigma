40bcfa44d691cec600893a803b64170b38e8e5fe
nabetse1320 <petermix.were@gmail.com>
2024-01-28 01:49:16 -0500

    a
    a

--- Assets/Scrips/CamaraAjustable.cs
+++ Assets/Scrips/CamaraAjustable.cs
@@ -1,19 +1,23 @@
-using Cinemachine;
 using UnityEngine;
+using Cinemachine;
 
-public class ZoomCamara : MonoBehaviour
+public class ZonaTrigger : MonoBehaviour
 {
     public CinemachineVirtualCamera camaraVirtual;
-    public float minFOV = 15f;
-    public float maxFOV = 60f;
-    public float sensibilidad = 10f;
+    public CinemachineVirtualCamera[] otrasCamaras;
 
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        if (other.gameObject.CompareTag("Player"))
         {
-            camaraVirtual.m_Lens.FieldOfView -= Input.GetAxis("Mouse ScrollWheel") * sensibilidad;
-            camaraVirtual.m_Lens.FieldOfView = Mathf.Clamp(camaraVirtual.m_Lens.FieldOfView, minFOV, maxFOV);
+            // Activar la cámara virtual de esta zona
+            camaraVirtual.Priority = 1;
+
+            // Desactivar todas las demás cámaras
+            foreach (var camara in otrasCamaras)
+            {
+                camara.Priority = 0;
+            }
         }
     }
 }
